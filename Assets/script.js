82d75056f51a690e9dd879b48ed26b867b751003
Austin Gore <awgore99@gmail.com>
2022-10-06 10:18:37 -0500

    Updated Javascript to fix errors when removing characters and add comments

--- Assets/script.js
+++ Assets/script.js
@@ -9,15 +9,18 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
 
-
-
+// function to generate the password
 function generatePassword(){
-  var charArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', '-', '?', '_', '@', '#', '%', '$', '^', '&', '*', '+', '=', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ]
-  var criteriaArray = []; 
+  //Array of all potential characters to be used
+  var charArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', '-', '?', '_', '@', '#', '%', '$', '^', '&', '*', '+', '=', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ];
+  // Array of characters to actually be used
+  var newCharArray = [];
+  // array to store characters for password
   var password = [];
+  // initial window prompt to ask for number of needed characters
   var passwordGenerator = window.prompt("How many characters would you like in the password")
+  //checking number of characters
   if (parseInt(passwordGenerator) > 128){
     window.alert("Too many characters, please try again");
   }
@@ -25,51 +28,42 @@ function generatePassword(){
     window.alert("Not enough characters, please try again");
   }
   else{
+    // add elements to password array as placeholders
     for (i = 0; i < parseInt(passwordGenerator); i++){
       password.push(" ");
     }
+    //Checking for each criteria, and removing undesired characters
     var lowChar = window.prompt("Should the password include lowercase characters?(Enter y for Yes and n for No)");
     if (lowChar.toLowerCase() === "n"){
-      charArray.remove('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+      newCharArray = charArray.splice(0, 25) + charArray.splice(51, charArray.length);
     }
     var upChar = window.prompt("Should the password include uppercase characters?(Enter y for Yes and n for No)");
     if (upChar.toLowerCase() === "n"){
-      charArray.remove('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+      newCharArray = charArray.splice(26, charArray.length);
     }
     var specChar = window.prompt("Should the password include special characters?(Enter y for Yes and n for No)");
     if (specChar.toLowerCase() === "n"){
-      charArray.remove('!', '-', '?', '_', '@', '#', '%', '$', '^', '&', '*', '+', '=');
+      newCharArray = charArray.splice(0, 50) + charArray.splice(65, charArray.length);
     }
     var numChar = window.prompt("Should the password include numeric characters?(Enter y for Yes and n for No)");
     if (numChar.toLowerCase() === "n"){
-      charArray.remove('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+      newCharArray = charArray.splice(0, 64);
     }
-    for (k = 0; k<criteriaArray.length; k++){
-
-    }
+    // assign desired characters to the password array
     for (j=0; j <password.length ; j++){
-      password[j] = charArray[getRandomInt(charArray.length)];
+      password[j] = newCharArray[getRandomInt(newCharArray.length)];
     }
+    // joining characters to make one password string
     password = password.join('');
   }
 
   return password;
   
 }
-
+// Random number generator
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
-
+// Event listener to initialize button and call the writePassword function
 generateBtn.addEventListener("click", writePassword);
 
-
-// declare initial array for each criteria
-// use a for loop to .prompt for each criteria asking yes or no
-//    modify array based on which are determined to be used
-//    use a for loop to iterate through the modified array and each selected criteria
-//      for each criteria, use if-else statements to manipulate the password
-//
-//
-//
-// generateBtn will be whats used to display the completed, randomized password
